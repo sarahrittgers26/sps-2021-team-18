784fb9aba517c329e8dd4c1dd04dda510763f7a6
Michael Lawes <mjalawes@gmail.com>
2021-03-24 20:54:42 -0400

    Add routing for authentication page and mui theme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+import Auth from './components/Auth/Auth';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Team 18</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <Route path="/" exact component={Auth} />
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#6E4AB1'
+    },
+    secondary: {
+      main: '#000000'
+    }
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontSize: 14,
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600
+  }
+})
