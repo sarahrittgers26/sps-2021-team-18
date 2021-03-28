6ed926118d292968cdff7928fad8a6e182b07391
Michael Lawes <mjalawes@gmail.com>
2021-03-27 20:45:18 -0400

    Delete backup files and duplicate files

--- src/components/Api/Api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-//const baseUrl = x === 'development' ? 'http://localhost:4000' : process.env.REACT_APP_API_URL;
-const baseUrl = 'http://localhost:4000'
-export default axios.create({
-	baseURL: baseUrl
-})
