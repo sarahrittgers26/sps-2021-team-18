686da92439374de3a2210e8f1976b4119e539cad
Mufaro Makiwa <mmakiwa@mit.edu>
2021-04-03 15:53:50 -0400

    Creates Projects page and active users section

--- target/classes/META-INF/resources/components/Projects/Projects.js
+++ target/classes/META-INF/resources/components/Projects/Projects.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Projects.css'
+import Users from './Users.js'
+
+const active = ["Mufaro Makiwa", "Cynthia Enofe", "Michael Lawes", "Sarah Rittgers"]
+const inactive = ["Andreea Lovan", "Emmanuel Makiwa"]
 
 function Projects() {
   return (
-    <div>
-      <div className="Projects_active">
+    <div className="Projects_container">
+
+      <div className="Projects_content">
 
       </div>
-      
+
+      <div className="Projects_active">
+        <Users 
+          active={active}
+          inactive={inactive}/>
+      </div>
     </div>
   )
 }
---
+++ target/classes/META-INF/resources/components/Projects/Users.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Users(props) {
+  
+  const {active, inactive} = props;
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Users
