4f9dd5d248615aa8a8fc4791b3b8340d9715c521
Mufaro Makiwa <mmakiwa@mit.edu>
2021-04-03 15:31:59 -0400

    Adds projects route to App.js

---
+++ src/main/webapp/components/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Projects.css'
+
+function Projects() {
+  return (
+    <div>
+      <div className="Projects_active">
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Projects
