8394c12e4d1923425681199ed87ad27628188335
Michael Lawes <mjalawes@gmail.com>
2021-04-04 19:51:14 -0400

    Removed user1Active and user2Active from Project schema, unnecessary

--- src/main/java/com/google/sps/data/Project.java
+++ src/main/java/com/google/sps/data/Project.java
@@ -5,16 +5,11 @@ public final class Project {
 	private final String user2;
 	private final String projectid;
 	private final String title;
-	private final boolean user1Active;
-	private final boolean user2Active;
 
-	public Project(String user1, String user2, String projectid, String title,
-			boolean user1Active, boolean user2Active) {
+	public Project(String user1, String user2, String projectid, String title) {
 		this.user1 = user1;
 		this.user2 = user2;
 		this.projectid = projectid;
 		this.title = title;
-		this.user1Active = user1Active;
-		this.user2Active = user2Active;
 	}
 }
