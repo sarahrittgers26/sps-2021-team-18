3427a07fb7cb3579c937b710e2a268a9131ecd52
Michael Lawes <mjalawes@gmail.com>
2021-05-06 18:04:30 +0000

    Updated files

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,17 @@
 # Build stage
 #
 FROM maven:3.6.0-jdk-11-slim AS build
-COPY src ../src
-COPY pom.xml ../
-RUN mvn -f ../pom.xml clean package
+
+WORKDIR /
+COPY src ./src
+COPY pom.xml .
+
+RUN mvn clean package
 
 #
 # Package stage
 #
 FROM openjdk:11-jre-slim
-COPY --from=build /target/portfolio-1.jar /portfolio-1.jar
+COPY --from=build /target/portfolio-*.jar /portfolio-1.jar
 EXPOSE 8080 9000
 CMD ["java","-jar","/portfolio-1.jar"]
--- src/main/webapp/middleware/socketMiddleware.js
+++ src/main/webapp/middleware/socketMiddleware.js
@@ -1,7 +1,9 @@
 import { baseUrl } from '../components/Api/Api';
 //const LOCAL = 'ws://localhost:9000/';
-const PRODUCTION = 'wss://chat.494901.xyz:9000/';
-const LOCAL = 'ws://34.75.137.84:9000/';
+//const PRODUCTION = 'wss://spring21-team-18-bexdlzuwhq-uc.a.run.app:9000/';
+//const PRODUCTION = 'wss://spring21-sps-18.appspot.com';
+const LOCAL = 'ws://localhost:9000/';
+const PRODUCTION = 'wss://spring21-sps-18.appspot.com/';
 //http://34.75.137.84/
 const AppConfig = {
   SOCKET: baseUrl === 'https://spring21-sps-18.appspot.com' ? PRODUCTION : LOCAL,
